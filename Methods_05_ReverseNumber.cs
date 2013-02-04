af4c595736310dc8cd952842b4a14207d2b4037e
noteto <nevtod@gmail.com>
2013-02-04 21:25:15 +0200

    Update Methods_05_ReverseNumber.cs

--- Methods_05_ReverseNumber.cs
+++ Methods_05_ReverseNumber.cs
@@ -36,3 +36,32 @@ namespace task16_methods06
         }
     }
 }
+
+//VARIANT 2 - for negative numbers
+
+//class ReverseDigits
+//{
+//    static void Main()
+//    {
+//        ReverseDecimal(decimal.Parse(Console.ReadLine()));
+//    }
+//    static void ReverseDecimal(decimal number)
+//    {
+//        string numberString = number.ToString(); //convert the input in string
+//        char[] numberChar = numberString.ToCharArray(); // then convert the string to array of chars
+//        char[] reversedNumberChar = new char[numberChar.Length];
+//        for (int i = 0; i < numberChar.Length; i++) // reverse the array of chars
+//        {
+//            reversedNumberChar[numberChar.Length - i - 1] = numberChar[i];
+//        }
+//        for (int i = 0; i < reversedNumberChar.Length; i++) // print the final array of chars. 45 is the ascii code of "-"
+//        {
+//            if (reversedNumberChar[i] == 45)
+//            {
+//                break; //if decimal is < 0 print the reversed number without "-" behind it
+//            }
+//            Console.Write(reversedNumberChar[i]);
+//        }
+//        Console.WriteLine();
+//    }
+//}
